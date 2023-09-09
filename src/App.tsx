6a0ad9fb7261542642584f2d4aab6fb63d03bc39
caiordev <caioreisdev@gmail.com>
2023-09-09 10:07:55 -0300

    feat: add thema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { LightThem } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightThem}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
