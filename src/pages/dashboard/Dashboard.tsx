0d7f93eaaf9530fc87d607ee59a8e24694949f82
caiordev <caioreisdev@gmail.com>
2023-09-11 14:50:17 -0300

    feat: add ferramentas de detalhes e add responsivi

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,11 @@
-import { FerramentasDaListagem } from "../../shared/components";
+import { FerramentasDeDetalhe } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePagina";
 
 export const Dashboard = () => {
   return (
     <LayoutBaseDePagina
       titulo="Página Inicial"
-      barraDeFerramentas={<FerramentasDaListagem mostrarInputBusca />}
+      barraDeFerramentas={<FerramentasDeDetalhe mostrarBotaoSalvarEFechar />}
     >
       Testando
     </LayoutBaseDePagina>
---
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
@@ -0,0 +1,189 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Icon,
+  Paper,
+  Skeleton,
+  Theme,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+
+interface IFerramentasDeDetalhe {
+  textoBotaoNovo?: string;
+  mostrarBotaoNovo?: boolean;
+  mostrarBotaoVoltar?: boolean;
+  mostrarBotaoApagar?: boolean;
+  mostrarBotaoSalvar?: boolean;
+  mostrarBotaoSalvarEFechar?: boolean;
+
+  mostrarBotaoSalvarCarregando?: boolean;
+  mostrarBotaoNovoCarregando?: boolean;
+  mostrarBotaoVoltarCarregando?: boolean;
+  mostrarBotaoApagarCarregando?: boolean;
+  mostrarBotaoSalvarEFecharCarregando?: boolean;
+
+  aoClicarEmNovo?: () => void;
+  aoClicarEmVoltar?: () => void;
+  aoClicarEmApagar?: () => void;
+  aoClicarEmSalvar?: () => void;
+  aoClicarEmSalvarEFechar?: () => void;
+}
+
+export const FerramentasDeDetalhe: React.FC<IFerramentasDeDetalhe> = ({
+  textoBotaoNovo = "novo",
+
+  mostrarBotaoApagar = true,
+  mostrarBotaoNovo = true,
+  mostrarBotaoSalvar = true,
+  mostrarBotaoSalvarEFechar = false,
+  mostrarBotaoVoltar = true,
+
+  aoClicarEmApagar,
+  aoClicarEmNovo,
+  aoClicarEmSalvar,
+  aoClicarEmSalvarEFechar,
+  aoClicarEmVoltar,
+
+  mostrarBotaoApagarCarregando = false,
+  mostrarBotaoNovoCarregando = false,
+  mostrarBotaoSalvarCarregando = false,
+  mostrarBotaoSalvarEFecharCarregando = false,
+  mostrarBotaoVoltarCarregando = false,
+}) => {
+  //smDown e mdDown trás valores booleanos
+  const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
+  const theme = useTheme();
+
+  return (
+    <Box
+      height={theme.spacing(5)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      gap={1}
+      alignItems="center"
+      component={Paper}
+    >
+      {mostrarBotaoSalvar && !mostrarBotaoSalvarCarregando && (
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          startIcon={<Icon>save</Icon>}
+          onClick={aoClicarEmSalvar}
+        >
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            Salvar
+          </Typography>
+        </Button>
+      )}
+
+      {mostrarBotaoSalvarCarregando && <Skeleton width={110} height={60} />}
+
+      {mostrarBotaoNovo && !mostrarBotaoNovoCarregando && !smDown && (
+        <Button
+          variant="outlined"
+          color="primary"
+          disableElevation
+          startIcon={<Icon>add</Icon>}
+          onClick={aoClicarEmNovo}
+        >
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          ></Typography>
+          {textoBotaoNovo}
+        </Button>
+      )}
+      {mostrarBotaoNovoCarregando && !smDown && (
+        <Skeleton width={110} height={61} />
+      )}
+
+      {mostrarBotaoSalvarEFechar &&
+        !mostrarBotaoSalvarEFecharCarregando &&
+        !smDown &&
+        !mdDown && (
+          <Button
+            variant="outlined"
+            color="primary"
+            disableElevation
+            startIcon={<Icon>save</Icon>}
+            onClick={aoClicarEmSalvarEFechar}
+          >
+            <Typography
+              variant="button"
+              whiteSpace="nowrap"
+              textOverflow="ellipsis"
+              overflow="hidden"
+            >
+              Salvar e voltar
+            </Typography>
+          </Button>
+        )}
+
+      {mostrarBotaoSalvarEFecharCarregando && !smDown && !mdDown && (
+        <Skeleton width={180} height={60} />
+      )}
+
+      {mostrarBotaoApagar && !mostrarBotaoApagarCarregando && (
+        <Button
+          variant="outlined"
+          color="primary"
+          disableElevation
+          startIcon={<Icon>delete</Icon>}
+          onClick={aoClicarEmApagar}
+        >
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            Apagar
+          </Typography>
+        </Button>
+      )}
+      {mostrarBotaoApagarCarregando && <Skeleton width={110} height={61} />}
+
+      {mostrarBotaoVoltar &&
+        (mostrarBotaoNovo ||
+          mostrarBotaoApagar ||
+          mostrarBotaoSalvar ||
+          mostrarBotaoSalvarEFechar) && (
+          <Divider variant="middle" orientation="vertical" />
+        )}
+
+      {mostrarBotaoVoltar && !mostrarBotaoVoltarCarregando && (
+        <Button
+          variant="outlined"
+          color="primary"
+          disableElevation
+          startIcon={<Icon>arrow_back</Icon>}
+          onClick={aoClicarEmVoltar}
+        >
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            Voltar
+          </Typography>
+        </Button>
+      )}
+      {mostrarBotaoVoltarCarregando && <Skeleton width={110} height={61} />}
+    </Box>
+  );
+};
