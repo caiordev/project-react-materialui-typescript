331464c3d4ab80451aa7a5ce9305952e48914e8f
caiordev <caioreisdev@gmail.com>
2023-09-23 11:50:36 -0300

    feat: add context of Auth

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,106 @@
+import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { FerramentasDeDetalhe } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePagina";
+import { CidadesService } from "../../shared/services/api/cidades/CidadesService";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
 export const Dashboard = () => {
+  const [isLoadingCidades, setIsLoadingCidades] = useState(true);
+  const [totalCountCidades, setTotalCountCidades] = useState(0);
+  const [isLoadingPessoas, setIsLoadingPessoas] = useState(true);
+  const [totalCountPessoas, setTotalCountPessoas] = useState(0);
+
+  useEffect(() => {
+    setIsLoadingCidades(true);
+    setIsLoadingPessoas(true);
+
+    CidadesService.getAll(1).then((result) => {
+      setIsLoadingCidades(false);
+      if (result instanceof Error) {
+        alert(result.message);
+      } else {
+        console.log(result);
+
+        setTotalCountCidades(result.totalCount);
+      }
+    });
+    PessoasService.getAll(1).then((result) => {
+      setIsLoadingPessoas(false);
+      if (result instanceof Error) {
+        alert(result.message);
+      } else {
+        console.log(result);
+
+        setTotalCountPessoas(result.totalCount);
+      }
+    });
+  }, []);
+
   return (
     <LayoutBaseDePagina
       titulo="Página Inicial"
-      barraDeFerramentas={<FerramentasDeDetalhe mostrarBotaoSalvarEVoltar />}
+      barraDeFerramentas={
+        <FerramentasDeDetalhe
+          mostrarBotaoNovo={false}
+          mostrarBotaoApagar={false}
+          mostrarBotaoSalvar={false}
+          mostrarBotaoVoltar={false}
+        />
+      }
     >
-      Testando
+      <Box width="100%" display="flex">
+        <Grid container margin={2}>
+          <Grid item container spacing={2}>
+            <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" align="center">
+                    Total de Cidades
+                  </Typography>
+                  <Box
+                    padding={6}
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    {!isLoadingCidades && (
+                      <Typography variant="h1">{totalCountCidades}</Typography>
+                    )}
+
+                    {isLoadingCidades && (
+                      <Typography variant="h5">Carregando...</Typography>
+                    )}
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" align="center">
+                    Total de Pessoas
+                  </Typography>
+                  <Box
+                    padding={6}
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    {!isLoadingPessoas && (
+                      <Typography variant="h1">{totalCountPessoas}</Typography>
+                    )}
+
+                    {isLoadingPessoas && (
+                      <Typography variant="h1">Carregando...</Typography>
+                    )}
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
     </LayoutBaseDePagina>
   );
 };
---
+++ src/shared/contexts/AuthContext.tsx
@@ -0,0 +1,61 @@
+import {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { AuthService } from "../services/api/auth/AuthService";
+
+interface IAuthContextData {
+  isAuthenticated: boolean;
+  login: (email: string, password: string) => Promise<string | void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
+const LOCAL_STORAGE_KEY_ACCESS_TOKEN = "APP_ACCESS_TOKEN";
+
+interface IAuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
+  const [accessToken, setAccessToken] = useState<string>();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN);
+    if (accessToken) {
+      setAccessToken(JSON.parse(accessToken));
+    } else {
+      setAccessToken(undefined);
+    }
+  }, []);
+
+  const handleLogin = useCallback(async (email: string, password: string) => {
+    const result = await AuthService.auth(email, password);
+    if (result instanceof Error) {
+      return result.message;
+    } else {
+      localStorage.setItem(
+        "APP_ACCESS_TOKEN",
+        JSON.stringify(result.acessToken)
+      );
+      setAccessToken(result.acessToken);
+    }
+  }, []);
+  const handleLogout = useCallback(() => {
+    localStorage.removeItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN);
+    setAccessToken(undefined);
+  }, []);
+
+  const isAuthenticated = useMemo(() => !!accessToken, [accessToken]);
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, login: handleLogin, logout: handleLogout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
